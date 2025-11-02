44d43490976a89886954643b4b5361c784ca4158
𝐏𝐲𝐌𝐚𝐊𝐚𝐢𝐳𝐮 <loconsotritditridomit@gmail.com>
2025-11-02 07:39:24 +0700

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,9 @@
+{
+  "id": "python.pymakaizu",
+  "name": "Python Code Checks",
+  "main": "main.js",
+  "version": "1.2.0",
+  "author": "MaZu!",
+  "description": "Phát hiện lỗi cú pháp Python, highlight và gợi ý sửa",
+  "files": ["main.js"]
+}
